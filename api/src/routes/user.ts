941572ff0da503d9e38b65b46faacd2d0ff857f8
akshit <akshitsadana@gmail.com>
2020-09-13 02:01:31 +0530

    created user routes

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,5 +1,18 @@
 import { Router } from 'express';
+import {
+    addUserController,
+    deleteUserController,
+    getAllUsersController,
+    getUserController,
+    updateUserController
+} from '../controllers';
 
 const router: Router = Router();
+
+router.post('/add', addUserController);
+router.get('/get/:userID', getUserController);
+router.get('/allUsers', getAllUsersController);
+router.patch('/update/:userID', updateUserController);
+router.delete('/delete/:userID', deleteUserController);
 
 export default router;
