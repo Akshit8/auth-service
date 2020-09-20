e0de5686ce5621357e0410853c467ba960635e39
akshit <akshitsadana@gmail.com>
2020-09-20 18:36:25 +0530

    created auth controllers

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -3,3 +3,11 @@ import { message, statusCode } from '../config';
 import { HttpError } from '../httpError';
 import { HttpResponse } from '../httpResponse';
 import { catchAsync } from '../middleware';
+
+export const loginController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+
+export const otpVerifyController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+
+export const resendController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+
+export const logoutController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
