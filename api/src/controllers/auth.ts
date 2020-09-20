184a0dbb91c0c39e321029c2b9041dd528a93ebc
akshit <akshitsadana@gmail.com>
2020-09-20 19:20:09 +0530

    login controller done

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -3,8 +3,18 @@ import { message, statusCode } from '../config';
 import { HttpError } from '../httpError';
 import { HttpResponse } from '../httpResponse';
 import { catchAsync } from '../middleware';
+import { User } from '../models';
+import { sendOtp } from '../utils';
 
-export const loginController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+export const loginController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const { phoneNumber } = req.body;
+    const user = await User.findOne({ phoneNumber });
+    if (!user) {
+        throw new HttpError(statusCode.badRequest, message.userNotExist);
+    }
+    sendOtp(user.phoneNumber);
+    next(new HttpResponse(statusCode.ok, null));
+});
 
 export const otpVerifyController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
 
