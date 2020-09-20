e6edc242aa6b215a283f2078fe4ddac29361073d
akshit <akshitsadana@gmail.com>
2020-09-20 17:40:34 +0530

    otp funcs completed

--- api/src/utils/otp.ts
+++ api/src/utils/otp.ts
@@ -9,23 +9,47 @@ const headers = {
 };
 
 export const sendOtp = async (phoneNumber: string) => {
+    axios({
+        method: 'get',
+        url: `${baseUrl}?authkey=${MSG91_AUTH_KEY}&template_id=${MSG91_TEMPLATE_ID}&mobile=${phoneNumber}&otp_length=${otpLength}&otp_expiry=${otpExpiry}`,
+        headers
+    })
+        .then((response) => {
+            logger.info(`otp.send.info ${JSON.stringify(response.data)}`);
+        })
+        .catch((error) => {
+            logger.error(`otp.send.error ${error}`);
+        });
+};
+
+export const verifyOtp = async (phoneNumber: string, otp: string) => {
     return new Promise((resolve, reject) => {
         axios({
-            method: 'get',
-            url: `${baseUrl}?authkey=${MSG91_AUTH_KEY}&template_id=${MSG91_TEMPLATE_ID}&mobile=${phoneNumber}&otp_length=${otpLength}&otp_expiry=${otpExpiry}`,
+            method: 'post',
+            url: `${baseUrl}/verify?otp_expiry=${otpExpiry}&mobile=${phoneNumber}&otp=${otp}&authkey=${MSG91_AUTH_KEY}`,
             headers
         })
             .then((response) => {
-                logger.info(`otp.send.info ${response}`);
-                resolve();
+                logger.info(`otp.verify.info ${JSON.stringify(response.data)}`);
+                resolve(response.data.type);
             })
             .catch((error) => {
-                logger.error(`otp.send.error ${error}`);
+                logger.error(`otp.verify.error ${error}`);
                 reject();
             });
     });
 };
 
-export const verifyOtp = async () => {
-    return new Promise((resolve, reject) => {});
+export const resendOtp = async (phoneNumber: string) => {
+    axios({
+        method: 'post',
+        url: `${baseUrl}/retry?mobile=${phoneNumber}&authkey=${MSG91_AUTH_KEY}`,
+        headers
+    })
+        .then((response) => {
+            logger.info(`otp.resend.info ${JSON.stringify(response.data)}`);
+        })
+        .catch((error) => {
+            logger.error(`otp.resend.error ${error}`);
+        });
 };
